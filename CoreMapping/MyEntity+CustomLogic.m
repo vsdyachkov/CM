08700971d4fa62a0e996194cf793e1a3d7fcd9c7
vsdyachkov <vdyachkov@nestline.com>
2014-08-28 21:05:24 +0400

    Fix repository

---
+++ CoreMapping/MyEntity+CustomLogic.m
@@ -0,0 +1,21 @@
+//
+//  MyEntity+CustomLogic.m
+//  CoreMapping
+//
+//  Created by Dyachkov Victor on 26.08.14.
+//  Copyright (c) 2014 Dyachkov Victor. All rights reserved.
+//
+
+/*
+#import "MyEntity+CustomLogic.h"
+
+@implementation MyEntity (CustomLogic)
+
+- (void) customizeWithJson: (NSDictionary*) json
+{
+    self.myBool = @YES;
+    //NSLog(@"self.myBool = %@", self.myBool);
+}
+
+@end
+*/
